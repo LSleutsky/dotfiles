4d47ba39793703806f5ffe0703424ac9ecfd4f24
Lush Sleutsky <lushsleutsky@gmail.com>
2022-07-25 00:57:30 -0400

    feat: add delete command for nvim download file

--- nvim.sh
+++ nvim.sh
@@ -2,3 +2,4 @@
 
 curl -LO https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
 tar -xzvf nvim-macos.tar.gz -C /usr/local/share/nvim --strip-components=1
+rm -rf ~/nvim-macos.tar.gz
