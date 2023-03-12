98a8f6ab1297628314931425a0aa3acee9c92357
Lush Sleutsky <lushsleutsky@gmail.com>
2023-03-12 15:38:25 -0400

    feat(latte): config for kde desktop

--- linux/nvim.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz
-tar -xzvf nvim-linux64.tar.gz -C /usr/local/share/nvim --strip-components=1
-rm -rf ~/nvim-linux64.tar.gz
