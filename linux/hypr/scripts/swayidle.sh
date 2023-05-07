f5de91960def3b7dea1dd3c697747d5c89cf327b
Lush Sleutsky <lushsleutsky@gmail.com>
2023-05-07 19:41:13 -0400

    fix: idle and hyprpicker

--- linux/hypr/scripts/swayidle.sh
+++ linux/hypr/scripts/swayidle.sh
@@ -2,7 +2,7 @@
 
 exec swayidle -w \
   timeout 900 "~/.config/hypr/scripts/swaylock.sh" \
-  timeout 1200 'swaymsg "hyprctl dispatch dpms off"' \
-  resume 'swaymsg "hyprctl dispatch dpms on"' \
+  timeout 1200 "hyprctl dispatch dpms off" \
+  resume "hyprctl dispatch dpms on" \
   before-sleep 'mpc pause' \
   before-sleep "~/.config/hypr/scripts/swaylock.sh"
