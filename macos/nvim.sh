45e6ba6697a7124a4a79d9775870adc33866c4e4
Lush Sleutsky <lushsleutsky@gmail.com>
2024-01-23 14:31:15 -0500

    build: update dotfiles hierarchy

--- macos/nvim.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-wget https://github.com/neovim/neovim/releases/download/nightly/nvim-macos.tar.gz
-xattr -c ~/nvim-macos.tar.gz
-tar -xzvf nvim-macos.tar.gz -C /usr/local/share/nvim --strip-components=1
-rm -rf ~/nvim-macos.tar.gz
